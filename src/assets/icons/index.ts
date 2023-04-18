58b2f34c36de785f0b23a05d96fe437e0d16a088
Asero0 <andrefelipeil@hotmail.com>
2023-04-18 15:21:09 -0300

    alretaçõoes index assets e add mouse.s vg

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import Mouse from './Mouse.svg';
-=======
 import Mouse from "./mouse.svg";
 import Arrow from './arrow.svg'
 
->>>>>>> feature/header
 export const ICONS = {
   Mouse,
   Arrow
