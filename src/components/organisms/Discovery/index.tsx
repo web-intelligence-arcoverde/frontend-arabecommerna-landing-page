514f2d9e525f8270f175ace3d7ecf3c454caabe1
Asero0 <andrefelipeil@hotmail.com>
2023-05-03 12:56:07 -0300

    fix: translation

--- src/components/organisms/Discovery/index.tsx
+++ src/components/organisms/Discovery/index.tsx
@@ -3,26 +3,20 @@ import { DiscoveryContent, DiscoveryImages, DiscoveryTitle } from './style';
 import { ICONS } from '@/assets';
 import { GroupButtons } from '@/components/atoms/ButtonsGroup';
 import { discoveryCultureMock } from '@/__mocks__';
+import { i18n } from '@/translate/i18n.js';
 
 export const Discovery = () => {
+  const subTitle = `${i18n.t('discovery.subTitle')}`;
   return (
     <DiscoveryContent>
       <DiscoveryTitle>
         <Image src={ICONS.Grade} />
-        <h1>DESCUBRA</h1>
+        <h1>{`${i18n.t('discovery.title')}`}</h1>
         <Image src={ICONS.Details} />
       </DiscoveryTitle>
-      <h2>
-        Porque aprender a <span>língua Árabe</span>
-      </h2>
+      <h2 dangerouslySetInnerHTML={{ __html: subTitle }}></h2>
       <GroupButtons />
-      <p>
-        A cultura árabe é extremamente rica e diversa, abrangendo mais de 20
-        países e uma população de mais de 400 milhões de pessoas. Essa riqueza
-        cultural é expressa em áreas como a literatura, arquitetura, música,
-        dança e gastronomia. A influência da cultura árabe pode ser vista em
-        várias partes do mundo, o que evidencia sua importância e valorização
-      </p>
+      <p>{`${i18n.t('discovery.text')}`}</p>
       <DiscoveryImages>
         {discoveryCultureMock.map((imgPresentation, index) => (
           <Image key={index} src={imgPresentation} />
