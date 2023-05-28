55e3b496df6bc4097f5499d3e872b8d8037ad2fd
João Paulo <joao.elesbao@g.globo.com>
2023-05-28 17:55:14 -0300

    fix:scroll  na section results

--- src/components/organisms/Results/style.ts
+++ src/components/organisms/Results/style.ts
@@ -13,6 +13,19 @@ export const ResultsContainer = styled.section`
   @media (max-width: ${BREAKPOINTS.sm}) {
     padding: 96px 20px;
     overflow-x: auto;
+    ::-webkit-scrollbar {
+    height: 6px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${COLORS.brand._02};
+    border-radius: 4px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${COLORS.brand._04};
+    border-radius: 4px;
+  }
   }
 `;
 
